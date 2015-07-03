63b1e034de9d8273978d91be51554e4992d60ab8
Fisher Adelakin <temidayoadelakin@gmail.com>
2015-07-03 19:04:04 -0400

    contacts contentprovider

--- app/src/main/java/com/fisheradelakin/bitchat/ContactsFragment.java
+++ app/src/main/java/com/fisheradelakin/bitchat/ContactsFragment.java
@@ -1,8 +1,11 @@
 package com.fisheradelakin.bitchat;
 
 import android.app.Activity;
+import android.database.Cursor;
+import android.provider.ContactsContract;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,6 +14,8 @@ import android.view.ViewGroup;
  * A placeholder fragment containing a simple view.
  */
 public class ContactsFragment extends Fragment {
+
+    private static final String TAG = "ContactsFragment";
 
     private Listener mListener;
 
@@ -21,6 +26,20 @@ public class ContactsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_contacts, container, false);
+
+        String[] columns  = {ContactsContract.CommonDataKinds.Phone.NUMBER, ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME};
+
+        Cursor cursor = getActivity().getContentResolver().query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
+                columns, null, null, null);
+        cursor.moveToFirst();
+        while(!cursor.isAfterLast()){
+            // get the names and numbers of people in my phone
+            String number = cursor.getString(0);
+            String name = cursor.getString(1);
+            Log.d(TAG, name +" "+number);
+            cursor.moveToNext();
+        }
+        cursor.close();
 
         return v;
     }
