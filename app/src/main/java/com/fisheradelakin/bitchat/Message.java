2014c4525ea712e19f8fe7c0d24697010c82e56f
Fisher Adelakin <temidayoadelakin@gmail.com>
2015-07-05 21:57:11 -0400

    add chatactivity

---
+++ app/src/main/java/com/fisheradelakin/bitchat/Message.java
@@ -0,0 +1,17 @@
+package com.fisheradelakin.bitchat;
+
+/**
+ * Created by Fisher on 7/5/15.
+ */
+public class Message {
+
+    private String mText;
+
+    public String getText() {
+        return mText;
+    }
+
+    public void setText(String text) {
+        mText = text;
+    }
+}
